f0252c11b1e947528b0df696b83cb86e1ab9c45e
Zubby <mrgabrielborgo@gmail.com>
2023-02-08 18:52:13 +0100

    emits, methods and shit

---
+++ vue-test/src/components/Button.vue
@@ -0,0 +1,27 @@
+<template>
+    <button @click="onClick()" :style="{ background: color }" class="btn">Add Task</button>
+</template>
+
+<script>
+import { type } from 'os';
+
+export default{
+    name:"Button",
+    props: {
+        text: {
+            type: String,
+            default: "Next",
+        },
+
+        color: {
+            type: String,
+            default: "black",
+        },
+    },
+    methods:{
+        onClick(){
+            alert("yo u clicked a button")
+        }
+    }
+}
+</script>
---
+++ vue-test/src/components/Header.vue
@@ -0,0 +1,38 @@
+<template>
+    <header>
+        <h1>Ciao {{ title }}</h1>
+        <!-- <h1>{{ name }}</h1> metodo 1 per usare i props in un component di vue -->
+        <Button color="red" text="New"/>
+        <Button  text="Update"/>
+
+    </header>
+</template>
+
+<script>
+import Button from "./Button.vue"
+
+export default {
+    name: 'Header',
+    // props: ["name", "address"], metodo 1 per avere props in un component di vue 
+    props: {
+        title: {
+            type: String,
+            default: "Not defined",
+            // valore default se non lo inseriamo, grazie al cazzo aggiungerei
+        },
+    },
+    components: {
+        Button
+    }
+    
+}
+</script>
+
+<style scoped>
+    header{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 20px;
+    }
+</style>
